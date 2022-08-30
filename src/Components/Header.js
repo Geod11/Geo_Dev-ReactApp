edb07be5870acb875eb1f9253cd0c9442dfce2c9
Geod11 <danilescugeorge@yahoo.com>
2022-08-30 16:27:32 +0200

    30 Aug - 16:27

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,7 +5,7 @@ return (
 <div className="Header-main-container">
     <img className='Logo' src='Geo_Dev.ico' alt='Logo' width={120}></img>
 <ul className="Header-main-list">
-<a href="https://www.sportingnews.com/es/nba?gr=www" target={"_blank"} className="Header-child-list" onClick={()=> console.log("Clicked")}>Home</a>
+<li className="Header-child-list">Home</li>
 <li className="Header-child-list">About</li>
 <li className="Header-child-list">Projects</li>
 <li className="Header-child-list">Skills</li>
