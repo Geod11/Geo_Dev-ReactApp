1fb872dbc856ca0dd3c410014e499b951c7971b1
Geod11 <danilescugeorge@yahoo.com>
2022-10-04 22:59:39 +0200

    4 Oct

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,19 +1,17 @@
-import React/*, { useState } */ from "react";
+import React, { useRef }/*, { useState } */ from "react";
 import "../Style_css/Contact.scss";
 import emailjs from 'emailjs-com';
 
 export default function Contact() {
 
-    // function MyForm() {
-    //     const [name, setName] = useState("");}
-
+    const form = useRef();
      function sendEmail(e) {
-        // const form = useRef();
+        
         e.preventDefault();
 
-        emailjs.sendForm('gmail', 'service_bd9rsvc', /*form.current*/ e.target, '2DEifTVyVNAAoXmob')
+        emailjs.sendForm('service_bd9rsvc', 'template_88i1uea', form.current, '2DEifTVyVNAAoXmob')
           .then((result) => {
-              console.log(result.text);
+              alert("Success")
           }, (error) => {
               console.log(error.text);
           });
@@ -24,23 +22,18 @@ export default function Contact() {
         <>
          <div className="contactPageContent">
          <h2>Hello, nice to meet you!</h2>
-          <form action="" onSubmit={sendEmail}>
+          <form ref={form} action="" onSubmit={sendEmail}>
            <div className="formWord">  
             <input className="input100" type="text" placeholder="Name:" name="name" required/>
             <br />   
             <input className="input100" type="text" placeholder="Phone:" name="phone" required/>
             <br />
-            <input className="input100" type="text" placeholder="Email:" name="email" required/>       
+            <input className="input100" type="text" placeholder="Email:" name="email" required pattern="/^[\w-\.]+@([\w-]+\.)+[a-z]{2,4}$/i"/>       
            </div>
-          </form> 
-           <br />
-          <div class="formWord2">
-           
            <textarea name="message" placeholder="Message content:" required rows="4" cols="50"></textarea>
-           
-          </div>  
-          <br />  
-          <button>SUBMIT</button>
+           <br />
+           <input className="submitButton" type="submit" value="Submit" />
+          </form>        
          </div>  
         </>
 )}
