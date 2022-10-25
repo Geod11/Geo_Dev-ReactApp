d428cba19e0267829344b81691a402237546fda5
Geod11 <danilescugeorge@yahoo.com>
2022-10-26 00:26:15 +0200

    Logo changes and completed Contact Page content

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,6 +6,18 @@ return (
 
 <ul className="Footer-main-list">
 <li className="Footer-child-list">Contact Info</li>
+<li className="Footer-child-list">
+<a href='https://www.linkedin.com/in/george-alexandru-danilescu-6b6aa456' target="_blank" rel="noopener noreferrer">
+<img className='LogoFooterLI' src='LinkedIn_Logo.ico' alt='LinkdeIn logo' width={40}>
+</img>
+</a>
+</li>
+<li className="Footer-child-list">
+<a href='https://github.com/Danilestii' target="_blank" rel="noopener noreferrer">
+<img className='LogoFooterGit' src='GitHub_Logo.ico' alt='GitHub logo' width={40}>
+</img>
+</a>
+</li>
 <li className="Footer-child-list">Phone Number: +34 644 302 652</li>
 <li className="Footer-child-list">Email address: georgedanilescu88@gmail.com</li>
 </ul>
--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -16,7 +16,7 @@ export default function Contact() {
               isMailSent(true);
               setTimeout(()=>{
                 isMailSent(false)
-              },4000);
+              },2000);
           }, (error) => {
               console.log(error.text);
           });
@@ -26,21 +26,20 @@ export default function Contact() {
     return (
         <>
          <div className="contactPageContent">
-         <h2>Hello, nice to meet you!!!</h2>
+         <h2>Hello, nice to meet you!</h2>
           <form ref={form} action="" onSubmit={sendEmail}>
            <div className="formWord">  
             <input className="input100" type="text" placeholder="Name:" name="name" required pattern="[a-zA-Z\s]+"/>
             <br />   
-            <input className="input100" type="text" placeholder="Phone:" name="phone" required pattern="[0-9+\s]+"/>
+            <input className="input100" type="text" placeholder="Phone:" name="phone" pattern="[0-9+\s]+"/>
             <br />
             <input className="input100" type="text" placeholder="Email:" name="email" required pattern="^\S+@\S+\.\S+$"/>       
            </div>
            <textarea name="message" placeholder="Message content:" required rows="4" cols="50"></textarea>
-           <br /><br />
+           <br />
            <input className="submitButton" type="submit" value="Submit" />
           </form>
           <div className={`emailSentBase ${mailSent===true && "emailSent"}`}> Email sent successfully! </div>
-          {mailSent===true && (<div className="mailSuccess"> Email sent successfully! </div>)}        
          </div>       
         </>
 )}
